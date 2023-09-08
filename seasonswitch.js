6f896c5312dd06d429a9c28352f433359dbbcd5a
swapnalisardesai <swapnali.sardesai@gmail.com>
2023-09-07 18:38:49 -0700

    added 1 file

---
+++ seasonswitch.js
@@ -0,0 +1,22 @@
+let month=5
+switch(month)
+{
+    case 1:
+    case 2:
+    case 11:
+    case 12:
+        console.log("winter season")
+        break;
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        console.log("summer season")
+        break;
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+        console.log("rainy season")
+        break;
+}
